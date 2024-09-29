476f671d99801adf92ca00f05196fa5b1585d45b
SebastianLorenzano <selorenzano3@gmail.com>
2024-09-29 20:07:24 +0200

    29-09-24

---
+++ Unity/IntroductionUnity/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] protected Transform prefabExplotion;
+    [SerializeField] protected Transform prefabAsteroid;
+    [SerializeField] private float ySpeed = -1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = mainCameraScript.X + 1 + Random.Range(0, mainCameraScript.Width - 1);
+        float y = mainCameraScript.Y + mainCameraScript.Height + 4;
+        transform.position = new Vector3(x, y, 0);
+        StartCoroutine(CreateAsteroid());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, ySpeed * Time.deltaTime, 0);
+        if (Collisions.IsOutOfBoundsSouth(gameObject))
+            Destroy(gameObject, 2f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.tag == "Player")
+            {
+                Transform explosion = Instantiate(prefabExplotion,
+                collision.transform.position, Quaternion.identity);
+                Destroy(explosion.gameObject, 1f);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private IEnumerator CreateAsteroid()
+    {
+
+        float pause = Random.Range(1.5f, 3.0f);
+        yield return new WaitForSeconds(pause);
+        Instantiate(prefabAsteroid, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
