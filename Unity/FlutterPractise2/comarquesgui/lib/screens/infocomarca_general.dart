d1d1ad237cb3c67dbc1303e337c7b862cbb3f516
SebastianLorenzano <selorenzano3@gmail.com>
2025-02-17 19:31:16 +0100

    17-02-25

--- Unity/FlutterPractise2/comarquesgui/lib/screens/infocomarca_general.dart
+++ Unity/FlutterPractise2/comarquesgui/lib/screens/infocomarca_general.dart
@@ -1,4 +1,5 @@
 import 'package:comarquesgui/models/comarca.dart';
+import 'package:comarquesgui/repository/repository_comarques.dart';
 import 'package:comarquesgui/repository/repository_exemple.dart';
 import 'package:flutter/material.dart';
 
@@ -12,55 +13,69 @@ class InfoComarcaGeneral extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Agafem la comarca del repositori
-    Comarca? comarca = RepositoryExemple.obtenirInfoComarca(comarcaString);
-    if (comarca == null) {
-      return Center(
-        child: Text("Comarca no encontrada"),
-      );
-    }
-    return Padding(
-      padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 50),
-            Image.network(comarca.img!,
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: 250,
-                fit: BoxFit.cover),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 45.0,
-                left: 20.0,
-                right: 20.0,
-                bottom: 20.0,
-              ),
-              child: Text(comarca.comarca,
-                  style: Theme.of(context).textTheme.headlineLarge),
+    return FutureBuilder(
+      future: RepositoryComarques.obtenirInfoComarca(comarcaString),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } if (snapshot.hasError) {
+          return Center(
+            child: Text("Error: ${snapshot.error}"),
+          );
+        }
+        Comarca? comarca = snapshot.data;
+        if (comarca == null) {
+          return Center(
+            child: Text("Comarca no encontrada"),
+          );
+        }
+        return Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 50),
+                Image.network(comarca.img!,
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    height: 250,
+                    fit: BoxFit.cover),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 45.0,
+                    left: 20.0,
+                    right: 20.0,
+                    bottom: 20.0,
+                  ),
+                  child: Text(comarca.comarca,
+                      style: Theme.of(context).textTheme.headlineLarge),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20.0,
+                    right: 20.0,
+                    bottom: 30.0,
+                  ),
+                  child: Text("Capital: ${comarca.capital!}",
+                      style: Theme.of(context).textTheme.headlineMedium),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20.0,
+                    right: 20.0,
+                  ),
+                  child: Text(comarca.desc!,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      textAlign: TextAlign.justify),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20.0,
-                right: 20.0,
-                bottom: 30.0,
-              ),
-              child: Text("Capital: ${comarca.capital!}",
-                  style: Theme.of(context).textTheme.headlineMedium),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20.0,
-                right: 20.0,
-              ),
-              child: Text(comarca.desc!,
-                  style: Theme.of(context).textTheme.headlineSmall,
-                  textAlign: TextAlign.justify),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
